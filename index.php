2099212a62f5e8228baaee393ea3afe626060d97
ER06 <107385997+ER06@users.noreply.github.com>
2022-06-18 02:50:39 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
-<HTML>
-<HEAD>
-<TITLE>ejemplo hola mundo</TITLE>
-</HEAD>
-<BODY>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda Virtual</title>
+</head>
+<body>
 <P>Hola Mundo</P>
-</BODY>
-</HTML>
+<?php echo "Hola Mundo en PHP"; 
+require_once "controladores/plantilla.controlador.php";
+$plantilla = new ControladorPlantilla();
+$plantilla -> plantilla();
+
+?>
+</body>
+</html>
 
 
